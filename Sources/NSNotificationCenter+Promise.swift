909451c108044a6c5a65284d373d5d9c87f52cf8
plin2 <ping-hsien.lin@vuud.com>
2016-09-20 09:29:58 -0700

    Added timeout

--- Sources/NSNotificationCenter+Promise.swift
+++ Sources/NSNotificationCenter+Promise.swift
@@ -21,25 +21,42 @@ import PromiseKit
 extension NotificationCenter {
     /// Observe the named notification once
     public func observe(once name: String) -> NotificationPromise {
-        let (promise, fulfill) = NotificationPromise.go()
+        let (promise, fulfill, _) = NotificationPromise.go()
         let id = addObserver(forName: NSNotification.Name(rawValue: name), object: nil, queue: nil, using: fulfill)
         _ = promise.then(on: zalgo) { _ in self.removeObserver(id) }
+        return promise
+    }
+    
+    public class func once(_ name: String) -> NotificationPromise {
+        return NotificationCenter.default.observe(once: name)
+    }
+    
+    public class func once(_ name: String, timeout: TimeInterval) -> NotificationPromise {
+        return NotificationCenter.default.observe(once: name)
+    }
+    
+    public func observe(once name: String, timeout: TimeInterval) -> NotificationPromise {
+        let (promise, fulfill, reject) = NotificationPromise.go()
+        let id = addObserver(forName: NSNotification.Name(rawValue: name), object: nil, queue: nil, using: fulfill)
+        after(interval: timeout).then { reject(NSError(code: 0)) }
+        _ = promise.always(on: zalgo) { _ in self.removeObserver(id) }
         return promise
     }
 }
 
 /// The promise returned by `NotificationCenter.observe(once:)`
-public class NotificationPromise: Promise<[AnyHashable: Any]> {
-    private let pending = Promise<Notification>.pending()
+open class NotificationPromise: Promise<[AnyHashable: Any]> {
+    fileprivate let pending = Promise<Notification>.pending()
 
-    public func asNotification() -> Promise<Notification> {
+    open func asNotification() -> Promise<Notification> {
         return pending.promise
     }
 
-    fileprivate class func go() -> (NotificationPromise, (Notification) -> Void) {
-        let (p, fulfill, _) = NotificationPromise.pending()
+    fileprivate class func go() -> (NotificationPromise, (Notification) -> (), (Error) -> () ) {
+        let (p, fulfill, reject) = NotificationPromise.pending()
         let promise = p as! NotificationPromise
         _ = promise.pending.promise.then { fulfill($0.userInfo ?? [:]) }
-        return (promise, promise.pending.fulfill)
+        _ = promise.pending.promise.catch { reject($0) }
+        return (promise, promise.pending.fulfill, promise.pending.reject)
     }
 }
